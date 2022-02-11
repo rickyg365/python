e6529913e5a144f6502e6c7b5db93a37856fc7bd
rickyg365 <rickygonzal3z@hotmail.com>
2022-02-10 22:50:55 -0800

    testing nvim setup

---
+++ web_scrap/data_handler.py
@@ -0,0 +1,18 @@
+import os
+
+from dataclasses import dataclasss
+from typing import List, Dict
+
+
+@dataclass
+class ComplexNum:
+    real: int
+    img: int
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
