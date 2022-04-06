cb463257279b8e88e6696e04e8e571545d1ce404
rickyg365 <rickygonzal3z@hotmail.com>
2022-04-06 16:09:09 -0700

    update file structure

--- recipe/main.py
+++ recipe/main.py
@@ -4,19 +4,28 @@ import json
 from models.recipe import Recipe
 from models.recipe_book import RecipeBook
 
+""" 
+Goals:
+- Input data
+- save and load as json
+- display data
+- analyze data?
+"""
 
 def load_data(filepath: str):
     # import data
     with open(filepath, 'r') as in_data:
-        data = json.load(in_data)
-
-    return data
+        loaded_data = json.load(in_data)
+    return loaded_data
 
 
 
 def main():
     test_data = load_data("data/test_data.json")
     print(test_data)
+
+    recipe = Recipe(**test_data)
+    print(recipe.full_str())
 
 if __name__ == '__main__':
     main()
