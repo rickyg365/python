07fbc9c0b0220709bf6a560c4de2e9caa52d3107
rickyg365 <rickygonzal3z@hotmail.com>
2022-06-22 09:43:31 -0700

    update display

--- data_entry/app.py
+++ data_entry/app.py
@@ -27,6 +27,26 @@ ___________________________________
 
 """
 
+def paged_display(data_list, entry_per_page: int):
+    """ Takes in list and diaplays it in a paged view! """
+    length = len(data_list)
+    current_page = 1
+    last_page = length/entry_per_page if length%entry_per_page == 0 else length//entry_per_page + 1
+    
+    start_index = 0
+    end_index = entry_per_page
+
+    current_display = ""
+    for _, entry in enumerate(data_list[start_index: end_index]):
+        
+
+        # Update Index    
+        start_index = (current_page - 1) * entry_per_page
+        end_index = current_page * entry_per_page
+    
+    return
+
+
 
 
 class DataEntryApp:
@@ -37,6 +57,10 @@ class DataEntryApp:
         self.path = default_path
         self.title = title
         self.data_choices = os.listdir(default_path)
+        self.num_choices = len(self.data_choices)
+
+        self.page = 1
+        self.last_page = self.num_choices//3 if self.num_choices%3 == 0 else self.num_choices//3 + 1
 
         self.chosen_dir = ""
         self.current_config = {}
@@ -47,24 +71,56 @@ class DataEntryApp:
     def __str__(self) -> str:
         # Choose by name
         # choices = "\n".join([f"+ {s.replace('_', ' ').title()}" for s in self.data_choices])
-        choices = ""
-        for _, dir in enumerate(self.data_choices[:min(3, len(self.data_choices))]):
+        choices = f""
+        self.num_choices = len(self.data_choices)  # Update
+        '''
+        '''
+        index = self.page - 1
+        # 0, 3
+        # 3, 6,
+        # 6, 9
+        start_index = 3 * index
+        end_index = 3 * self.page
+        
+        # start_index = 3 * index + index
+        # end_index = 3 * self.page + index
+        
+        for _, dir in enumerate(self.data_choices[start_index: end_index]):
             nice_dir_name = dir.replace("_", " ").title()
-            choices += f"\n{_+1}. {nice_dir_name}"
+            choices += f"{start_index + _+1}. {nice_dir_name}\n"
+        
+
+        # for _, dir in enumerate(self.data_choices[:min(3, self.num_choices)]):
+        #     nice_dir_name = dir.replace("_", " ").title()
+        #     if self.num_choices > 3:
+        #         choices += f"[N]: Next Page\n{_+1}. {nice_dir_name}"
+        #         continue
+        #     choices += f"\n{_+1}. {nice_dir_name}"
         
         # config: 3  data: 23
 
         txt = f"""
 [ {self.title} ]
 {30*'_'}
-config: {len(self.current_config)}  data: {len(self.current_data)}
+Pg.{self.page}  data: {self.chosen_dir}
+{30*'-'}
 {choices}
-{30*'_'}
+<Prev Page   Next Page>
+{30*'-'}
 [E]: Edit Data
 [C]: Create New
 [V]: View Data
 """
         return txt
+    
+    def edit_current_data(self):
+        print_list = ["\n".join([f"{k}: {v}" for k,v in e.items()]) for e in self.current_data]
+        # for entry in self.current_data:
+        #     for k, v in entry.items():
+        #         print(f"{k}: {v}")
+        print("\n".join(print_list))
+        input()
+        select_key = input("Select Key: ")
     
     def save_current_data(self):
         new_path = f"{self.path}/{self.chosen_dir}"
@@ -127,6 +183,28 @@ config: {len(self.current_config)}  data: {len(self.current_data)}
 
             if u_in.lower() == "c":
                 self.create_new()
+                continue
+            
+            # Prev Data Dir Page
+            if u_in.lower() == "p":
+                next_index = self.page - 1
+
+                if next_index < 0:
+                    next_index = 0
+                    
+                self.page = next_index
+
+                continue
+
+            # Next Data Dir Page
+            if u_in.lower() == "n":
+                next_index = self.page + 1
+
+                if next_index > self.last_page:
+                    next_index = self.last_page
+
+                self.page = next_index
+                continue
 
             if u_in.lower() == 'v':
                 print_list = ["\t".join([f"{k}: {v}" for k,v in e.items()]) for e in self.current_data]
@@ -135,6 +213,7 @@ config: {len(self.current_config)}  data: {len(self.current_data)}
                 #         print(f"{k}: {v}")
                 print("\n".join(print_list))
                 input()
+                continue
 
             if u_in in "123456789":
                 key = int(u_in) - 1
@@ -142,6 +221,7 @@ config: {len(self.current_config)}  data: {len(self.current_data)}
 
                 self.load_data_choice(self.data_choices[key])
                 # print(self.data_choices[key])
+                continue
 
         return
 
