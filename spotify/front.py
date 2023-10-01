23fd150f6ebf3dbab04d461ce7b475210a776990
rickyg365 <rickygonzal3z@hotmail.com>
2023-09-30 17:48:31 -0700

    updated python/spotify project

---
+++ spotify/front.py
@@ -0,0 +1,89 @@
+import os
+
+"""
+[Page 1]
+Song     Artist    Genre     Duration    Date
+Africa   Toto      Classic   1:23        09/23/23
+Africa   Toto      Classic   1:23        09/23/23
+Africa   Toto      Classic   1:23        09/23/23
+Africa   Toto      Classic   1:23        09/23/23
+Africa   Toto      Classic   1:23        09/23/23
+* type song name to select
+[P]rev Page  [N]ext Page  [G]oto page #
+>>> 
+
+"""
+# UI Variables
+SONG_GAP = 15
+ARTIST_GAP = 10
+GENRE_GAP = 10
+DURATION_GAP = 10
+DATE_GAP = 10
+
+
+def flatten_entry(data):
+    NAME = data.get("song", "N/A")
+    ARTIST = data.get("artist", "N/A")
+    GENRE = data.get("genre", "N/A")
+    DURATION = data.get("duration", "N/A")
+    DATE = data.get("date", "N/A")
+    return f"{NAME:<{SONG_GAP}}{ARTIST:<{ARTIST_GAP}}{GENRE:<{GENRE_GAP}}{DURATION:<{DURATION_GAP}}{DATE:<{DATE_GAP}}"
+
+
+def songs_page(data):
+    page = f"{'Song':<{SONG_GAP}}{'Artist':<{ARTIST_GAP}}{'Genre':<{GENRE_GAP}}{'Duration':<{DURATION_GAP}}{'Date':<{DATE_GAP}}"
+    for entry in data:
+        new_line = flatten_entry(entry)
+        page += f"\n{new_line}"
+
+    return page
+
+
+# App Variables
+CURRENT_PAGE = 0
+
+SINGLE_ENTRY = {
+    "song": "Africa",
+    "artist": "Toto",
+    "genre": "Classic",
+    "duration": 123,
+    "date": "09/23/23"
+}
+SAMPLE_DATA = [
+    {
+        "song": "Africa",
+        "artist": "Toto",
+        "genre": "Classic",
+        "duration": 123,
+        "date": "09/23/23"
+    },
+    {
+        "song": "Africa",
+        "artist": "Toto",
+        "genre": "Classic",
+        "duration": 123,
+        "date": "09/23/23"
+    },
+    {
+        "song": "Africa",
+        "artist": "Toto",
+        "genre": "Classic",
+        "duration": 123,
+        "date": "09/23/23"
+    },
+    {
+        "song": "Africa",
+        "artist": "Toto",
+        "genre": "Classic",
+        "duration": 123,
+        "date": "09/23/23"
+    }
+]
+
+
+def app():
+    print(songs_page(SAMPLE_DATA))
+
+
+if __name__ == "__main__":
+    app()
