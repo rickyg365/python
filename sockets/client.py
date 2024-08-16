a019c369877783d5c8735bc88ae55d42990ffc30
Ricardo Gonzalez <67453883+rickyg365@users.noreply.github.com>
2024-08-16 14:22:39 -0700

    Quicksave

--- sockets/client.py
+++ sockets/client.py
@@ -6,9 +6,7 @@ load_dotenv('.env')
 
 # Local host: 127.0.0.1
 IP = os.environ.get('IP')
-PORT = os.environ.get('PORT')
-
-
+PORT = int(os.environ.get('PORT'))
 
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
